a905674175625f7ddc129f41761b179bc82f940d
Kirito <102875535+KiritoGamesPlays@users.noreply.github.com>
2023-12-30 23:09:37 -0300

    Create status.js

---
+++ src/pages/api/status.js
@@ -0,0 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ aplicações: '45', clientes: "21", avaliações: "20" })
+}
