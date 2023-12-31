c81919786808ff64b76b38292ec76b8888058478
Kirito <102875535+KiritoGamesPlays@users.noreply.github.com>
2023-12-30 23:04:58 -0300

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'Speed Cloud', description: "Speed Cloud Melhor Hospedagem De Bots, Apis, Sites e Servidores De Minecraft Com a Melhor Latência Do Mercado" })
 }
